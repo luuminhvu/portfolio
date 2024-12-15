533d0ba47f51160fcf7925c49835ae5c3c8ec5e0
luuminhvu <https://github.com/luuminhvu>
2024-12-15 18:21:31 +0700

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <p className="text-blue-500">Hello World</p>;
+  return (
+    <>
+      <section className="h-full">
+        <div className="container mx-auto h-full">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            <div className="text-center xl:text-left">
+              <span className="">Web Developer</span>
+              <h1 className="h1">
+                Hello, I&apos;m <br />
+                <span className="text-accent">Luu Van Vu</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
